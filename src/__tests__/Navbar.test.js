400c2689d460a460b4255c9e3740661aa2f1b47c
wolterbwambale <benplaan1@gmail.com>
2023-07-05 16:48:55 +0300

    add test case for navbar

---
+++ src/__tests__/Navbar.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../components/navigation/Navbar';
+
+test('Navbar matches snapshot', () => {
+  const { container } = render(
+    <Router>
+      <Navbar />
+    </Router>
+  );
+  expect(container).toMatchSnapshot();
+});
+
